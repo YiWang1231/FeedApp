a24495df669db9c46880cc448b1cf2b2d9bb85a0
PeterWang <wangyihusky@163.com>
2022-05-16 00:40:10 +0800

    优化了tabItem的初始化，后续维护更方便 实现了viewPager与tabItem联动

--- app/src/main/java/com/peter/feedapp/MainActivity.kt
+++ app/src/main/java/com/peter/feedapp/MainActivity.kt
@@ -2,14 +2,21 @@ package com.peter.feedapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.peter.feedapp.adapter.FragmentAdapter
 import com.peter.feedapp.fragment.FragmentMain
 import com.peter.feedapp.fragment.FragmentMe
 import com.peter.feedapp.fragment.FragmentQuestion
 import com.peter.feedapp.fragment.FragmentSystem
+import com.peter.feedapp.model.TabGroup
+import com.peter.feedapp.model.TabItem
 
 class MainActivity : AppCompatActivity() {
     private val titleIDs = intArrayOf(
@@ -17,25 +24,30 @@ class MainActivity : AppCompatActivity() {
         R.string.question_page_title,
         R.string.system_page_title,
         R.string.me_page_title)
-    private lateinit var mainFragment : FragmentMain
+    private val iconResourcesIDs = intArrayOf(
+        R.drawable.ic_bottom_bar_home,
+        R.drawable.ic_bottom_bar_ques,
+        R.drawable.ic_bottom_bar_navi,
+        R.drawable.ic_bottom_bar_mine
+    )
+    private  var tabList: MutableList<View> = ArrayList()
+    private lateinit var tabGroup: TabGroup
+    private lateinit var mainFragment: FragmentMain
     private lateinit var questionFragment : FragmentQuestion
     private lateinit var systemFragment : FragmentSystem
     private lateinit var meFragment : FragmentMe
-    private  var fragments : MutableList<Fragment> = ArrayList()
+    private  var fragments: MutableList<Fragment> = ArrayList()
     private lateinit var containerContent : ViewPager
     private lateinit var mainNaviTab : LinearLayout
     private lateinit var questionNaviTab : LinearLayout
     private lateinit var systemNaviTab : LinearLayout
     private lateinit var meNaviTab : LinearLayout
-    private  var tabGroup : MutableList<LinearLayout> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initView()
-        initFragments()
-        initContent()
-        setClickListener()
+        setTabClickListener()
     }
 
     /**
@@ -43,16 +55,11 @@ class MainActivity : AppCompatActivity() {
      */
     private fun initView() {
         containerContent = findViewById(R.id.container_content)
-        mainNaviTab = findViewById(R.id.navi_main)
-        questionNaviTab = findViewById(R.id.navi_question)
-        systemNaviTab = findViewById(R.id.navi_system)
-        meNaviTab = findViewById(R.id.navi_me)
-        tabGroup.add(mainNaviTab)
-        tabGroup.add(questionNaviTab)
-        tabGroup.add(systemNaviTab)
-        tabGroup.add(meNaviTab)
+        initTabLayout()
+        initFragments()
+        initContent()
         // 初始化选定主页tab
-        selectItem(0)
+        tabGroup.selectTab(0)
     }
 
     /**
@@ -63,15 +70,24 @@ class MainActivity : AppCompatActivity() {
         val adapter = FragmentAdapter(supportFragmentManager, fragments)
         containerContent.adapter = adapter
         // TODO 设定监听实现与tab联动
-    }
+        containerContent.addOnPageChangeListener(object : OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                //TODO("Not yet implemented")
+            }
 
-    /***
-     * 选中tab 修改样式
-     */
-    private fun selectItem(pos: Int) {
-        for ( index in tabGroup.indices) {
-            tabGroup[index].isSelected = index == pos
-        }
+            override fun onPageSelected(position: Int) {
+                tabGroup.selectTab(position)
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                //TODO("Not yet implemented")
+            }
+
+        })
     }
 
     /***
@@ -89,13 +105,22 @@ class MainActivity : AppCompatActivity() {
         fragments.add(meFragment)
     }
 
+    private fun initTabLayout() {
+        tabGroup = TabGroup(this)
+        tabGroup.group = findViewById(R.id.page_navi)
+        for (index in titleIDs.indices) {
+            val tabItem: TabItem = TabItem(titleIDs[index], iconResourcesIDs[index])
+            tabGroup.addTabItem(tabItem)
+        }
+    }
+
     /***
      * 点击监听
      */
-    private fun setClickListener() {
-        for ((index, tabItem) in tabGroup.withIndex()) {
+    private fun setTabClickListener() {
+        for ((index, tabItem) in tabGroup.tabList.withIndex()) {
             tabItem.setOnClickListener {
-                selectItem(index)
+                tabGroup.selectTab(index)
                 containerContent.setCurrentItem(index, true)
             }
         }
